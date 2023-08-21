14475506adc07677af3d93f18015ac9d0f5f7fc6
Claudio Santos <122230401+claudiosssant@users.noreply.github.com>
2023-08-21 19:52:53 -0300

    Add files via upload
    First program with C#

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.Design;
+
+int num1, num2, resultado = 0;
+
+string operacao;
+
+Console.WriteLine("Informe o primeiro número");
+num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe o segundo número");
+num2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe a operação");
+operacao = Console.ReadLine();
+
+if(operacao == "+")
+{
+    resultado = num1 + num2;
+}
+else if(operacao == "-")
+{
+    resultado = num1 - num2;
+}
+else if(operacao == "*")
+{
+    resultado = num1 * num2;
+}
+else if(operacao == "/")
+{
+    resultado = num1 / num2;
+}
+
+Console.WriteLine(resultado);
